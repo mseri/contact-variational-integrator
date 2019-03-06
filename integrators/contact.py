daf3d8f94499a7d2e2060e3584cfffb471b25d65
Mats Vermeeren <33064142+mvermeeren@users.noreply.github.com>
2019-03-06 11:07:51 +0100

    Added a 4th order method
    Added the method dEL to contact.py, which returns the integrator corresponding to a given discrete Lagrangian.
    Used this to implement a 4th order method for the damped oscillator

--- integrators/contact.py
+++ integrators/contact.py
@@ -1,7 +1,44 @@
 import numpy as np
+import sympy as sp
+import scipy.optimize as so
 
 from integrators.common import getsteps
 
+def dEL(lag, implicit = True):
+    """
+    Return the integrator corresponding to a given discrete Lagrangian.
+    If implicit = False, a symbolic solution to the EL equations is sought.
+    """
+    p,x0,x1,z0,z1,h = sp.symbols("p x0 x1 z0 z1 h")
+    p0 = sp.expand( - h * sp.diff(lag(x0,x1,z0,z1,h),x0) / ( 1 + h * sp.diff(lag(x0,x1,z0,z1,h),z0) ) )
+    p1 = sp.expand( h * sp.diff(lag(x0,x1,z0,z1,h),x1) / ( 1 - h * sp.diff(lag(x0,x1,z0,z1,h),z1) ) )
+    
+    if implicit:
+        xequation = sp.lambdify([p,x0,x1,h], p0 - p) #equation to be solved for x1 at each step
+    else:
+        pold = sp.symbols("pold")
+        [xformula] = sp.solve(p0 - pold,x1) #symbolic expression for next x
+        nextx = sp.lambdify([pold,x0,h],xformula)
+        
+    peq = sp.lambdify([x0,x1,h],p1)
+    
+    def integrator(init, tspan, stepsize):
+        steps = getsteps(tspan, stepsize)
+        t0, _ = tspan
+
+        sol = np.empty([steps, 2], dtype=np.float64)
+        sol[0] = np.array(init)
+        for i in range(steps-1):
+            p, x = sol[i]
+            if implicit:
+                [xnew] = so.fsolve(lambda xnew: xequation(p,x,xnew,stepsize), x + stepsize*p)
+            else:
+                xnew = nextx(p,x,stepsize)
+            pnew = peq(x,xnew,stepsize)
+            sol[i+1] = np.array((pnew, xnew))
+        return sol
+    
+    return integrator
 
 def contact(init, tspan, h, a, acc, forcing):
     """
